f23217293180993dddd6f072bddb3a54fa43f700
NicolePell <pellicena@gmail.com>
2015-03-11 13:57:08 +0000

    all scenario tests passing

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -6,7 +6,10 @@ describe('shopping app', function() {
 
   beforeEach(function() {
     browser.get('index.html');
-    productList = element.all(by.tagName('h3'));
+    productList = element.all(by.repeater('product in products'));
+    basketList = element.all(by.repeater('item in basket'));
+    addButton = element.all(by.className('add-to-basket')).get(0);
+    removeButton = element.all(by.className('remove-from-basket')).get(0);
   });
 
   it('has a list of products', function() {
@@ -14,61 +17,31 @@ describe('shopping app', function() {
     expect(productList.get(2).isPresent()).toBe(true);
   });
 
-  it('the shop name links back to the homepage', function() {
-    element(by.id('brand')).click();
-    expect(browser.getCurrentUrl()).toMatch('http://localhost:8000/app/index.html#')
-  });
-
   it('has an Add to Basket button for each product', function() {
     expect(element(by.className('add-to-basket')).isPresent()).toBe(true);
     expect(element(by.className('add-to-basket')).getText()).toEqual('Add to Basket');
   });
 
-  it('displays a notice when a user clicks the add to basket button', function() {
-    element(by.className('add-to-basket')).click();
+  it('has a basket', function() {
+    expect(element(by.css('.basket')).isPresent()).toBe(true);
+  });
 
+  it('as a user I can add a product to the basket', function() {
+    addButton.click();
+    expect(basketList.count()).toEqual(1);
+  });
+
+  it('as a user I can see the total price of my basket', function() {
+    addButton.click();
+    addButton.click();
+    expect(element(by.css('.total')).getText()).toEqual('Your Basket | Total: £198.00');
+  });
+
+  it('as a user I can remove a product from my basket', function() {
+    addButton.click();
+    expect(basketList.count()).toEqual(1);
+    removeButton.click();
+    expect(basketList.count()).toEqual(0);
   });
 
 });
-
-// it('by clicking on the "Add a new contact" i should be redirected to another page', function(){
-//   element(by.className('addContact')).click();
-//   expect(browser.getCurrentUrl()).toContain('/new')
-// });
-//
-// it('after clicking to add a new contact I should be presented with a form', function(){
-//   element(by.className('addContact')).click();
-//   expect(element(by.tagName('h1')).getText()).toEqual('Add a new contact');
-//   expect(element(by.tagName('form')).isPresent()).toBe(true);
-// });
-//
-// it('should be able to add a contact by filling up the form', function(){
-//   element(by.className('addContact')).click();
-//   element(by.id('exampleInputSurname1')).sendKeys('Zzzz')
-//   element(by.id('exampleInputName1')).sendKeys('Zzzz')
-//   element(by.className('submit_newContact')).click()
-//   element.all(by.repeater('contact in contacts')).then(function(rows) {
-//     var lastRow = rows[rows.length -1]
-//     expect(lastRow.getText()).toMatch('ZZZZ ZZZZ');
-//   })
-// });
-//
-// it('should have an edit button available for each contact', function(){
-//   expect(element(by.className('editZzzzZzzz')).isPresent()).toBe(true);
-// });
-//
-// it('should have the edit form prefilled', function(){
-//   element(by.className('editZzzzZzzz')).click()
-//   expect(element(by.id('exampleInputSurname1')).getAttribute('value')).toMatch('Zzzz')
-//   expect(element(by.id('exampleInputName1')).getAttribute('value')).toMatch('Zzzz')
-// });
-//
-// it('should be able to edit a contact', function(){
-//   element(by.className('editZzzzZzzz')).click()
-//   element(by.id('exampleInputName1')).clear()
-//   element(by.id('exampleInputName1')).sendKeys('Aaaa')
-//   element(by.className('submit_editContact')).click()
-//   element.all(by.repeater('contact in contacts')).then(function(rows) {
-//     var lastRow = rows[rows.length -1]
-//     expect(lastRow.getText()).toMatch('ZZZZ AAAA Edit Delete');
-//   })
